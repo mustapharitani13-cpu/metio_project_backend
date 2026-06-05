29ae1818ad015fbf1184120ae9f73d7d3e6a8655
Mustapha-X <mustapharitani13@gmail.com>
2026-06-05 22:23:17 +0200

    Fix SSL: add certifi for MongoDB Atlas connection in Docker

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 # backend/database.py
 import os
+import certifi
 from motor.motor_asyncio import AsyncIOMotorClient
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://127.0.0.1:27017")
 
-client = AsyncIOMotorClient(MONGO_URL)
+client = AsyncIOMotorClient(MONGO_URL, tlsCAFile=certifi.where())
 
 db = client["metio_chatbot_db"]
 
