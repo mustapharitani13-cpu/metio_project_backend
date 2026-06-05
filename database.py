9eb544ecc826e7d1936574d0623479763c675e96
Mustapha-X <mustapharitani13@gmail.com>
2026-06-05 22:56:37 +0200

    Fix SSL: use Python 3.10 + bypass TLS cert verification for Atlas

--- database.py
+++ database.py
@@ -5,7 +5,16 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://127.0.0.1:27017")
 
-client = AsyncIOMotorClient(MONGO_URL, tlsCAFile=certifi.where())
+# Use tlsAllowInvalidCertificates for cloud deployment compatibility
+if "mongodb+srv" in MONGO_URL or "mongodb.net" in MONGO_URL:
+    client = AsyncIOMotorClient(
+        MONGO_URL,
+        tls=True,
+        tlsAllowInvalidCertificates=True,
+        tlsCAFile=certifi.where()
+    )
+else:
+    client = AsyncIOMotorClient(MONGO_URL)
 
 db = client["metio_chatbot_db"]
 
